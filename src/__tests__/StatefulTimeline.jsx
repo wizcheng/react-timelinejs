448ef0fe85def52472adcb7f7e207aa73583ffc4
wizcheng <wizcheng@gmail.com>
2018-07-22 01:47:35 +0800

    Extract shared code in tests

---
+++ src/__tests__/StatefulTimeline.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {mockData, mockLines, mockRange} from "./Timeline.tester";
+import Timeline from "../Timeline";
+
+export class StatefulTimeline extends React.Component {
+
+    constructor(props){
+        super(props);
+        const data = mockData({size: 3});
+        this.state = {
+            data: data,
+            lines: mockLines({}),
+            range: mockRange(data)
+        }
+    }
+
+    updateMockData = () => {
+        const data = mockData({size: 4});
+        this.setState({
+            data: data,
+            lines: mockLines({}),
+            range: mockRange(data)
+        });
+    };
+
+    render() {
+
+        const {data, lines, range} = this.state;
+        return (
+            <div>
+                <button id='update-mock-data' onClick={this.updateMockData}>generate</button>
+                <Timeline data={data} dataKey='key' lines={lines} range={range}/>
+            </div>
+        )
+    }
+
+
+}
--- src/__tests__/TimelineProperties.test.js
+++ src/__tests__/TimelineProperties.test.js
@@ -1,42 +1,5 @@
 import React from 'react';
-import Timeline from '../Timeline';
-import {mockData, mockLines, mockRange} from './Timeline.tester';
-
-class StatefulTimeline extends React.Component {
-
-    constructor(props){
-        super(props);
-        const data = mockData({size: 3});
-        this.state = {
-            data: data,
-            lines: mockLines({}),
-            range: mockRange(data)
-        }
-    }
-
-    updateMockData = () => {
-        const data = mockData({size: 4});
-        this.setState({
-            data: data,
-            lines: mockLines({}),
-            range: mockRange(data)
-        });
-    };
-
-    render() {
-
-        const {data, lines, range} = this.state;
-        return (
-            <div>
-                <button id='update-mock-data' onClick={this.updateMockData}>generate</button>
-                <Timeline data={data} dataKey='key' lines={lines} range={range}/>
-            </div>
-        )
-    }
-
-
-}
-
+import {StatefulTimeline} from "./StatefulTimeline";
 
 test('render update data properties', () => {
     const wrapper = mount(<StatefulTimeline/>);
